952118e71009ac5b6df46d04eafd26a21390a58a
Santiago Da Silva <sdasilva@outlook.es>
2021-01-02 22:52:31 -0300

    first commit

---
+++ src/test/java/net/folktag/cardatabase/CardatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package net.folktag.cardatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
