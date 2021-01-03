cb470e3c71016d37e805c76685955b75964c949c
Santiago Da Silva <sdasilva@outlook.es>
2021-01-02 23:34:00 -0300

    add comment, import Logger and LoggerFactory

--- src/main/java/net/folktag/cardatabase/CardatabaseApplication.java
+++ src/main/java/net/folktag/cardatabase/CardatabaseApplication.java
@@ -1,13 +1,19 @@
 package net.folktag.cardatabase;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CardatabaseApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 
 	public static void main(String[] args) {
+		// After adding this comment the application is restarted
 		SpringApplication.run(CardatabaseApplication.class, args);
+		logger.info("Hello Spring Boot");
 	}
 
 }
